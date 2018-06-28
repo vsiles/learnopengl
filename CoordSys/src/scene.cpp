b90056b76a91fc2b52d5860288dcc0d576123123
Vincent Siles <vincent.siles@provenrun.com>
2018-06-28 16:15:32 +0200

    CoordSys: toying with model matrix: more cubes

--- CoordSys/src/scene.cpp
+++ CoordSys/src/scene.cpp
@@ -154,6 +154,19 @@ void Scene::run()
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glEnable(GL_DEPTH_TEST);
 
+    static glm::vec3 cubePositions[] = {
+        glm::vec3( 0.0f,  0.0f,  0.0f),
+        glm::vec3( 2.0f,  5.0f, -15.0f),
+        glm::vec3(-1.5f, -2.2f, -2.5f),
+        glm::vec3(-3.8f, -2.0f, -12.3f),
+        glm::vec3( 2.4f, -0.4f, -3.5f),
+        glm::vec3(-1.7f,  3.0f, -7.5f),
+        glm::vec3( 1.3f, -2.0f, -2.5f),
+        glm::vec3( 1.5f,  2.0f, -2.5f),
+        glm::vec3( 1.5f,  0.2f, -1.5f),
+        glm::vec3(-1.3f,  1.0f, -1.5f)
+    };
+
     while (true) {
         bool done = false;
         while (SDL_PollEvent(&event) != 0) {
@@ -198,15 +211,20 @@ void Scene::run()
 
         glBindVertexArray(VAO);
 
-        GLfloat time = SDL_GetTicks() / 1000.0f;
+        /* Setup View matrix */
+        glm::mat4 view = glm::mat4(1.0f);
+        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+
+        /* Projection matrix is updated when needed */
+        shader.setMat4("projection", glm::value_ptr(projection));
+        shader.setMat4("view", glm::value_ptr(view));
+
+        for(unsigned int i = 0; i < 10; i++)
         {
             glm::mat4 model = glm::mat4(1.0f);
-            model = glm::rotate(model, time * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
-            glm::mat4 view = glm::mat4(1.0f);
-            view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-
-            shader.setMat4("projection", glm::value_ptr(projection));
-            shader.setMat4("view", glm::value_ptr(view));
+            model = glm::translate(model, cubePositions[i]);
+            GLfloat angle = 20.0f * i;
+            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
             shader.setMat4("model", glm::value_ptr(model));
 
             glDrawArrays(GL_TRIANGLES, 0, 36);
