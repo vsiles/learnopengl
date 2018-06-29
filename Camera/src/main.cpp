fd0c7b0b2594d942604d8711fafd7ac9168f3d43
Vincent Siles <vincent.siles@provenrun.com>
2018-06-29 14:51:01 +0200

    Camera: use glm::lookAt to create the same scene

---
+++ Camera/src/main.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include "scene.hpp"
+
+using namespace std;
+using namespace vinz;
+
+#define WIDTH   800
+#define HEIGHT  600
+
+static bool get_resource_path(string &path)
+{
+    char *basePath = SDL_GetBasePath();
+    if (basePath == nullptr) {
+        cerr << "SDL_GetBasePath() failed: " << SDL_GetError() << endl;
+        return false;
+    }
+
+    path = basePath;
+    SDL_free(basePath);
+
+    /* We replace the last bin/ with res/ to get the the resource path */
+    size_t pos = path.rfind("bin");
+    path = path.substr(0, pos) + "resources/";
+    return true;
+}
+
+int main(int argc, char **argv)
+{
+    (void)argc;
+    (void)argv;
+
+    int ret = EXIT_FAILURE;
+    try {
+        Scene scene("Hello GL", WIDTH, HEIGHT);
+        string path;
+        if (!get_resource_path(path))
+            return EXIT_FAILURE;
+
+        if (!scene.init(path))
+            return EXIT_FAILURE;
+        scene.run();
+        ret = EXIT_SUCCESS;
+    } catch (SceneFailure &excp) {
+        switch (excp.cause) {
+            case SceneFailure::Cause::Init:
+                cerr << "SDL_Init() failed: " << excp.str << endl;
+                break;
+            case SceneFailure::Cause::Window:
+                cerr << "SDL_CreateWindow() failed: " << excp.str << endl;
+                break;
+            case SceneFailure::Cause::Context:
+                cerr << "SDL_GL_CreateContext() failed: " << excp.str << endl;
+                break;
+            case SceneFailure::Cause::Extension:
+                cerr << "gl3wInit() failed: " << excp.str << endl;
+                break;
+        }
+    }
+    return ret;
+}
---
+++ Camera/src/scene.hpp
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <SDL.h>
+#include <string>
+#include <memory>
+#include <vector>
+
+#include "glm/mat4x4.hpp"
+
+#include "main.hpp"
+#include "shader.hpp"
+#include "texture.hpp"
+
+struct SceneFailure {
+    enum class Cause {
+        Init, Window, Context, Extension
+    };
+    Cause cause;
+    const char *str;
+    SceneFailure(Cause cause, const char *str):cause(cause),str(str) {};
+};
+
+namespace vinz {
+    class Scene {
+        private:
+            SDL_Window *window;
+            SDL_GLContext glcontext;
+            int width;
+            int height;
+            ShaderProgram shader;
+            Texture tex0, tex1;
+            glm::mat4 projection;
+
+            void updateProjView();
+
+        public:
+            Scene() = delete;
+            Scene(std::string name, int width, int height);
+            virtual ~Scene();
+
+            /* Forbid copy or assignment */
+            Scene(Scene const &) = delete;
+            Scene(Scene &&) = delete;
+            Scene& operator=(Scene const &) = delete;
+            Scene& operator=(Scene &&) = delete;
+
+            void run();
+            bool init(std::string &res_path);
+    };
+};
